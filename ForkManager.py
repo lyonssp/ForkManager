d32a01fde314fbbac5a0a39d67041ad7f14cc5bd
lyonssp <lyonssp@clarkson.edu>
2016-02-16 23:16:41 -0500

    Adding isParent() and isChild() methods with tests

--- ForkManager.py
+++ ForkManager.py
@@ -1,32 +1,50 @@
-import posix,sys,os
+import posix,sys,os,pprint
+
+pp = pprint.PrettyPrinter(indent=4)
+pp_err = pprint.PrettyPrinter(indent=4,stream=sys.stderr)
 
 class ForkManager():
+
+    ###PUBLIC###
     def onStart(self,func):
         self.onStartFunc = func
+
+    def onFinish(self,func):
+        self.onFinishFunc = func
+
+    def start(self):
+        if self.__parent_pid__ == os.getpid():
+            pid = posix.fork()
+        else:
+            pid = 0
+
+        self.__this_pid__ = os.getpid()
+        if pid: #TODO check existence of start function
+            try:
+                self.__runOnStartFunc__( self.__this_pid__ )
+            except Exception as e:
+                pp_err.pprint( e )
+        return pid
+    
+    def finish(self):
+        try:
+            self.__runOnFinishFunc__( self.__this_pid__ )
+        except Exception as e:
+            pp_err.pprint( e )
+        sys.exit()
+
+    def isParent(self):
+        return self.__parent_pid__ == self.__this_pid__
+    
+    def isChild(self):
+        return self.__parent_pid__ != self.__this_pid__
+   
+    ###PRIVATE### 
+    __parent_pid__ = os.getpid()
 
     def __runOnStartFunc__(self,pid):
         self.onStartFunc(pid)
         
-    def onFinish(self,func):
-        self.onFinishFunc = func
-
     def __runOnFinishFunc__(self,pid):
         self.onFinishFunc(pid)
 
-    def start(self):
-        pid = posix.fork()
-        if pid: #TODO check existence of start function
-            try:
-                self.this_pid = os.getpid()
-                self.__runOnStartFunc__( self.this_pid )
-            except Exception:
-                pass 
-        return pid
-    
-    def finish(self):
-        pid = os.getpid()
-        try:
-            self.onFinishFunc(pid)
-        except Exception:
-            pass
-        sys.exit()
--- test/testSuite.py
+++ test/testSuite.py
@@ -1,16 +1,23 @@
-import sys,os
+import sys,os,unittest
 sys.path.append('../')
+
 from time import sleep
 from ForkManager import ForkManager
-import unittest
 from mock import patch , ANY
+
 
 fm = ForkManager()
 def onStartFunc(pid):
     return
 
+def onFinishFunc(pid):
+    return
+
 fm.onStart( 
     onStartFunc
+)
+fm.onFinish(
+    onFinishFunc
 )
 
 class TestForkManagerModule( unittest.TestCase):
@@ -28,11 +35,24 @@ class TestForkManagerModule( unittest.TestCase):
     
     #Test that finish() ends the child process like it should
     def testFinish(self):
-        pid = fm.start() 
+        pid = fm.start()
         if not pid:
             with self.assertRaises(SystemExit):
                 fm.finish()
                 self.assertTrue(False) #should not get here
+
+    #Test that isParent and isChild work 
+    #correctly in both parent and child proc
+    def testParentChildChecks(self):
+        pid = fm.start()
+        if pid:
+            self.assertTrue( fm.isParent() )
+            self.assertFalse( fm.isChild() )
+        else:
+            self.assertTrue( fm.isChild() )
+            self.assertFalse( fm.isParent() )
+            with self.assertRaises(SystemExit):
+                fm.finish()
 
 if __name__ == '__main__':
     unittest.main()
